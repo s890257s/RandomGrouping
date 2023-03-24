6f1f92c2cea617fec0aeb964b0ad3a14149f9599
Allen <s890257s@yahoo.com.tw>
2023-03-24 12:52:21 +0800

    feature:新增保存資料功能
    在左下角新增保存資料的按鈕與對應的功能；
    資料會保存在localStorage，
    尚未完成清除資料的功能，
    且有「左上角頁數CSS不會隨著改變」的Bug。

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const assignBTN = document.getElementById("assignBTN");
 const clearBTN = document.getElementById("clearBTN");
 const groupHome = document.getElementById("groupHome");
 const groupCountBTNs = document.getElementById("groupCountBTNs");
+const saveDataBTNs = document.getElementsByClassName("savaDataBTNs")[0];
+let finalGroups = []; //最終分組
+let isExample = true;
+
 
 //初始化
 (function initialize() {
@@ -15,11 +19,14 @@ const groupCountBTNs = document.getElementById("groupCountBTNs");
   //填入預設的【分組用按鈕】
   let btns = "";
   for (let i = 4; i <= 7; i++) {
-    btns += `<button class="groupCountBTN ${
-      i == 4 ? "active" : ""
-    }">${i}</button>`;
+    btns += `<button class="groupCountBTN ${i == 4 ? "active" : ""
+      }">${i}</button>`;
   }
   groupCountBTNs.innerHTML = btns;
+
+  //讀取並渲染保存的資料
+  let t, saveDatas = (t = localStorage.getItem("saveDatas")) === null ? [] : JSON.parse(t);
+  renderSaveData(saveDatas);
 })();
 
 //分組邏輯(照順序分組 → 打亂每組順序 → 從每組中逐一抓人，丟到最終分組名單)
@@ -28,6 +35,7 @@ assignBTN.addEventListener("click", function () {
   let memberSize = memberArray.length; //總人數
   let tG = +totalGroups.value; //總組數
   let groupOfOrder = []; //第一次分組陣列
+  finalGroups = []; //歸零最終分組陣列
 
   //如果沒有輸入值，終止程式
   if (tG <= 0 || memberArray[0] == "") {
@@ -45,8 +53,6 @@ assignBTN.addEventListener("click", function () {
   });
 
   //逐一從每組中抓人，放到最終分組名單
-  let finalGroups = [];
-
   for (let i = 0; i < tG; i++) {
     finalGroups.push([]);
     for (let j = 0; j < groupOfOrder.length; j++) {
@@ -54,22 +60,10 @@ assignBTN.addEventListener("click", function () {
     }
   }
 
-  //頁面渲染
-  let outputData = "";
-  for (let i = 1; i <= finalGroups.length; i++) {
-    outputData += '<div class="groupCard">';
-    outputData += `<p>第${i}組</p>`;
-    outputData += '<div><textarea cols="30" rows="12">';
-    finalGroups[i - 1].forEach((m) => {
-      if (m != undefined) {
-        outputData += m + "\n";
-      }
-    });
-    outputData += "</textarea></div>";
-    outputData += "</div>";
+  renderPage();
+  if (!isExample) {
+    saveData();
   }
-
-  groupHome.innerHTML = outputData;
 });
 
 //執行預設方法
@@ -97,13 +91,37 @@ function shuffle(array) {
   }
 }
 
+//頁面渲染
+function renderPage() {
+
+  let outputData = "";
+  for (let i = 1; i <= finalGroups.length; i++) {
+    outputData += '<div class="groupCard">';
+    outputData += `<p>第${i}組</p>`;
+    outputData += '<div><textarea cols="30" rows="12">';
+    finalGroups[i - 1].forEach((m) => {
+      if (m != undefined) {
+        outputData += m + "\n";
+      }
+    });
+    outputData += "</textarea></div>";
+    outputData += "</div>";
+  }
+  groupHome.innerHTML = outputData;
+}
+
 // 清空分組名單
 clearBTN.addEventListener("click", () => {
-  Array.from(document.getElementsByTagName("textarea")).forEach((ta) => {
+  Array.from(document.getElementsByTagName("textarea")).forEach(ta => {
     ta.innerHTML = "";
     ta.value = "";
   });
 });
+
+//清除所有資料
+function removeAllData() {
+
+}
 
 // 判斷總組數input是否為 4 - 7
 totalGroups.addEventListener("input", () => {
@@ -114,3 +132,54 @@ totalGroups.addEventListener("input", () => {
       : btns[i].classList.remove("active");
   }
 });
+
+//產生唯一識別UUID
+function getUUID() {
+  var d = Date.now();
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+    d += performance.now(); //use high-precision timer if available
+  }
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+}
+
+//切換【示範】狀態
+memberHome.addEventListener("change", () => {
+  isExample = false;
+})
+
+//保存分組資料
+function saveData() {
+
+  let t, saveDatas = (t = localStorage.getItem("saveDatas")) === null ? [] : JSON.parse(t);
+
+  saveDatas.push({ id: getUUID(), groups: finalGroups });
+
+
+  localStorage.setItem('saveDatas', JSON.stringify(saveDatas));
+
+  renderSaveData(saveDatas);
+}
+
+//渲染分組資料按鈕
+function renderSaveData(saveDatas) {
+  let outPutBTNs = "";
+  saveDatas.forEach(s => {
+    outPutBTNs += `<button class='saveDataBTN' id='${s.id}'>${s.id.slice(0, 6)}</button>`;
+  })
+  saveDataBTNs.innerHTML = outPutBTNs;
+}
+
+//分組資料按鈕的點擊事件
+saveDataBTNs.addEventListener('click', function (e) {
+
+  if (e.target.classList.contains("saveDataBTN")) {
+    targetGroup = JSON.parse(localStorage.getItem("saveDatas")).filter(item => item.id == e.target.id)[0]
+    finalGroups = targetGroup.groups;
+    totalGroups.value = targetGroup.groups.length;
+    renderPage();
+  }
+})
